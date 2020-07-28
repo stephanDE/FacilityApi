3f3504954c64e1ce934e472520bd5b7f442311ea
Stephan Deist <sd@fino.digital>
2020-07-28 10:05:34 +0200

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -30,7 +30,7 @@ export class AuthController {
       throw new BadRequestException('Password is required');
     }
     return this.httpService.post(
-      'auth.keycloak/auth/realms/edu/protocol/openid-connect/token',
+      'https://keycloak.auth/auth/realms/edu/protocol/openid-connect/token',
       {
         grant_type: 'password',
         client_id: 'university-service',
